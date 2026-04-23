76c0b272b33ef7a40f058497ba911b12ba441dce
sujal-sakpal <sakpalsujal121@gmail.com>
2026-04-24 01:08:41 +0530

    cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=[settings.frontend_url],
+	allow_origins=["https://prep-up.vercel.app"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
